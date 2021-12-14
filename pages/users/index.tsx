3b8425f3e57ed99bc2e5f9bf73d0b63a90c64a32
daffajulistyo <daffajulistyo88@gmail.com>
2021-12-14 11:37:29 +0700

    First Commit Yoo

---
+++ pages/users/index.tsx
@@ -0,0 +1,10 @@
+import Layout from '../../components/Layout';
+
+export default function Users() {
+    return (
+        <Layout>
+            <p>Enter Your Name</p>
+        </Layout>
+         
+    )
+}
